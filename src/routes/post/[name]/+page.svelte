863c25e5f067fd0fd83ddcdfb0bf01cdc4a5cf5a
Script Raccoon <scriptraccoon@gmail.com>
2023-05-01 12:14:20 +0200

    remove else

--- src/routes/post/[name]/+page.svelte
+++ src/routes/post/[name]/+page.svelte
@@ -1,27 +1,30 @@
 <script lang="ts">
-  import { page } from "$app/stores";
+	import { page } from "$app/stores";
 
-  const name = $page.params.name;
+	const name = $page.params.name;
 
-  const path_object = import.meta.glob("/src/lib/posts/*.svelte");
+	const path_object = import.meta.glob("/src/lib/posts/*.svelte");
 
-  const key = `/src/lib/posts/${name}.svelte`;
+	const key = `/src/lib/posts/${name}.svelte`;
 
-  let post_component: ConstructorOfATypedSvelteComponent;
+	let post_component: ConstructorOfATypedSvelteComponent;
 
-  async function load_component() {
-    const import_function = path_object[key];
-    if (!import_function) return;
-    const module = await import_function();
-    if (module && typeof module === "object" && "default" in module)
-      post_component = module.default as ConstructorOfATypedSvelteComponent;
-  }
+	async function load_component() {
+		const import_function = path_object[key];
+		if (!import_function) return;
+		const module = await import_function();
+		if (
+			module &&
+			typeof module === "object" &&
+			"default" in module
+		)
+			post_component =
+				module.default as ConstructorOfATypedSvelteComponent;
+	}
 
-  load_component();
+	load_component();
 </script>
 
 {#if post_component}
-  <svelte:component this={post_component} />
-{:else}
-  <h1>No post found</h1>
+	<svelte:component this={post_component} />
 {/if}
